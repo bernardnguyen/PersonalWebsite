393721826b377f90319d23ab1ee86ebfaa62642b
bernardnguyen <bernardquynguyen@gmail.com>
2017-08-26 16:29:48 -0700

    testing without .html

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-var edu = require('./edu/edu.js');
-var exp = require('./exp/exp.js');
-var proj = require('./proj/proj.js');
-var rand = require('./rand/rand.js');
+var edu = require('./edu');
+var exp = require('./exp');
+var proj = require('./proj');
+var rand = require('./rand');
 
 
 /************** MIDDLEWARE ***************/
@@ -33,9 +33,7 @@ app.get('/experiences', logreq, sendData);
 app.get('/projects', logreq, sendData);
 app.get('/random', logreq, sendData);
 
-//app.use(express.static(__dirname + '/'));
-
-app.use(express.static(path.join(__dirname, 'public'),{index:false,extensions:['html']}));
+app.use(express.static(__dirname + '/'));
 
 
 
