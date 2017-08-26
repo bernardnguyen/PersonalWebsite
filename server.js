bc0fc59ec672b33f5733d2959386695729acfe23
bernardnguyen <bernardquynguyen@gmail.com>
2017-08-26 16:50:43 -0700

    testing without .html

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
-var edu = require('./edu/edu.js');
-var exp = require('./exp/exp.js');
-var proj = require('./proj/proj.js');
-var rand = require('./rand/rand.js');
+
+var edu = require('./edu');
+var exp = require('./exp');
+var proj = require('./proj');
+var rand = require('./rand');
 
 
 /************** MIDDLEWARE ***************/
 
-function logreq(req, res, next){
-  console.log(req.method + req.url);
-  next();
-}
+// function logreq(req, res, next){
+//   console.log(req.method + req.url);
+//   next();
+// }
 
-function sendData(req, res, next){
-  if (req.url === '/education'){
-    res.send(edu);
-  } else if (req.url === '/experiences'){
-    res.send(exp);
-  } else if (req.url === '/projects'){
-    res.send(proj);
-  } else if (req.url === '/random'){
-    res.send(rand);
-  }
-}
+// function sendData(req, res, next){
+//   if (req.url === '/education'){
+//     res.send(edu);
+//   } else if (req.url === '/experiences'){
+//     res.send(exp);
+//   } else if (req.url === '/projects'){
+//     res.send(proj);
+//   } else if (req.url === '/random'){
+//     res.send(rand);
+//   }
+// }
 
 /************* DEFINE ROUTES *************/
 
-app.get('/education', logreq, sendData);
-app.get('/experiences', logreq, sendData);
-app.get('/projects', logreq, sendData);
-app.get('/random', logreq, sendData);
+// app.get('/education', logreq, sendData);
+// app.get('/experiences', logreq, sendData);
+// app.get('/projects', logreq, sendData);
+// app.get('/random', logreq, sendData);
 
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
+
+app.use('/education', edu);
+app.use('/experiences', exp);
+app.use('/projects', proj);
+app.use('/random', rand);
 
 /************ LISTEN ON PORT **************/
 
