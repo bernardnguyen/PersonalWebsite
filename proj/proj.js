f86c7d85bc8d231210f3efc496c158f5bca69dbb
bernardnguyen <bernardquynguyen@gmail.com>
2017-08-26 16:21:09 -0700

    testing without .html

--- proj/proj.js
+++ proj/proj.js
@@ -2,4 +2,17 @@ if (typeof document != 'undefined'){
 
 var div = document.getElementById('main');
 
+/*PROJECT IDEAS:
+ * 
+ * Desktop
+ * Coffee Table
+ * Watkins research
+ * CSI
+ * 
+ */
+
+
+
+
+
 }
