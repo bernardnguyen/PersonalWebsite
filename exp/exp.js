32017a7c36f19c096f7d15b95a450f4d85aa8607
bernardnguyen <bernardquynguyen@gmail.com>
2017-08-27 22:23:40 -0700

    finished exp

--- exp/exp.js
+++ exp/exp.js
@@ -7,37 +7,35 @@ var div = document.getElementById('main');
 
 addExperience('witshelp', 'Help Desk Technician', 'Willamette Integrated Technology Services (WITS)', '(December 2013-May 2016)');
 var help = document.getElementById('witshelp-details');
-addParagraph(help, 'HELP DESK STUFF');
-addParagraph(help, 'HELP DESK STUFF');
-addParagraph(help, 'HELP DESK STUFF');
+addParagraph(help, 'Growing up, I found myself frequently reading articles, reviews, and watching youtube videos on the latest technology. As a kid, it usually wasn\'t anything I could have afforded, but I enjoyed learning about the latest developments in technology nonetheless. When a friend originally suggested for me to apply to WITS, I thought I wasn\'t good enough. I didn\'t know anything about fixing computers. I went into the interview and told my soon-to-be boss that I had no coding experience (a common misconception, I\'d later learn). In fact, one of the student workers sitting in on the interview (and my future roommate), later told me that I barely made the cut. Today, I can\'t imagine what my life would be like had I never gotten that job.');
+addParagraph(help, 'Right off the bat, I started to absorb information, gathering knowledge faster than I could believe. Within a couple months I went from being too timid to touch someone\'s computer to fully confident in my abilities to repair one on my own. My work with WITS was a confirmation of sorts. Working with technology was what I always wanted to do. I enjoyed how quickly I learned on the job. My boss and older student workers gave me the freedom to figure out problems on my own, but were also always available for guidance when needed. I started to spend more time in the office, even when I wasn\'t officially on shift. Coworkers became friends and the office quickly became a great place to eat lunch or waste time between classes. My boss loved having us around, even if only because we were available to help when things got busy.');
+addParagraph(help, 'WITS offered me my first experience of a fun, but functional work culture. When things weren\'t busy, office antics were common such as making hot dogs out of starbursts or putting an image of a koala on every checkout laptop after reimaging. Students at the very least didn\'t hate going to work, but for the most part, we actually enjoyed it. As a result, students were always willing to work, finding shift coverage was rarely an issue, and we were all learning skills that would be valuable for the rest of our lives. I learned quickly, worked hard, and started to stand out. After the end of my first semester working, a graduating senior emailed me saying there was an opening in the "tech shop". I knew only about 2 or 3 students worked in the tech shop, and although it was actually just a second job, I looked at it as a sort of promotion. After all, I had access to more hours, was given much more responsibility, and only got this opportunity because of the hard work I put in.');
 
 
 /************* TECH SHOP **************/
 
 addExperience('witstech', 'Tech Shop Assistant', 'Willamette Integrated Technology Services (WITS)', '(August 2014-May 2016)');
 var tech = document.getElementById('witstech-details');
-addParagraph(tech, 'TECH SHOP STUFF');
-addParagraph(tech, 'TECH SHOP STUFF');
-addParagraph(tech, 'TECH SHOP STUFF');
+addParagraph(tech, 'The tech shop is responsible for all the behind-the-scenes tech support on campus. They manage any university-owned hardware from professor\'s computers to classroom technology to emergency telephones across campus. Working in the basement of a dorm building with intelligent and quirky individuals was reminiscent of The IT Crowd. When I walked in for my first shift, I remember being told "Every day will have something new in the tech shop." Looking back, that was a fairly accurate statement.');
+addParagraph(tech, 'Some of my friends may think the only perk of my new job was being able to drive a golf cart around campus, but the job offered me quite the unique experience. I\'ve fixed things in buildings undergraduate students have never stepped foot in. I\'ve visited closets, basements, and roofs that my fellow students didn\'t know existed. I was the first person to unbox a brand new computer, I ran network cable in newly renovated buildings, and I even installed an experimental access point in the living room of my own university apartment. I once held a set of keys for every door on campus and went on a field trip to replace the router at the university\'s nearby farm. My ID card gave me access to every building on campus during the day, and every academic building at night. Working in the tech shop was a great opportunity to learn a lot, but it was also a unique opportunity for a very unique student experience.');
 
 
 /************* WUSTL **************/
 
 addExperience('wustl', 'Introduction to Electrical Engineering', 'Washington University in St. Louis', '(December 2014-January 2015)');
 var wustl = document.getElementById('wustl-details');
-addParagraph(wustl, 'JTERM');
-addParagraph(wustl, 'JTERM');
-addParagraph(wustl, 'JTERM');
+addParagraph(wustl, 'One of the required classes to get into Columbia University\'s dual-degree program was an Introductory Electrical Engineering course. Unfortunately, Willamette didn\'t offer any such course, but Washington University in St. Louis (another of Willamette\'s affiliated schools) did. In my sophomore year, I decided to attend WUSTL\'s J-Term offering over winter break and take their course. It was a way to not only complete that course, but to get a feel for both electrical engineering and the WUSTL campus/community. Completing a semester\'s material in 2 weeks was obviously a daunting task. That was my first experience being around individuals who not only worked tirelessly, but through teamwork and companionship encouraged me to do the same. After all, what kind of students willingly give up their winter break to study engineering instead? (Especially right after Christmas and through the New Year)');
+addParagraph(wustl, 'I actually genuinely enjoyed taking classes for 8 hours a day, and working on assignments for 5 hours at night. There was something special about all of us going through the experience together. We ate breakfast together, went to class together, ate lunch together, worked through labs together, then went home and ate dinner and did our homework together. When we had a little free time, we drank together, and sat by the hotel suite\'s fireplace roasting marshmellows together. Growing up, I was always told that "it\'s not work if you enjoy what you\'re doing." That was the first time I realized that it also doesn\'t feel like work if you enjoy the people you\'re working with.');
 
 
 /********** CODESMITH **************/
 
 addExperience('codesmith', 'Summer Academy of Code', 'Codesmith, LA', '(August 2017)');
 var code = document.getElementById('codesmith-details');
-addParagraph(code, 'CODESMITH STUFF');
-addParagraph(code, 'CODESMITH STUFF');
-addParagraph(code, 'CODESMITH STUFF');
-
+addParagraph(code, 'I applied to Codesmith\'s Summer Academy of Code as a backup plan if I didn\'t get a summer internship. To be honest, I was a little mislead by the wording in the email they sent, and thought it was a scholarship opportunity. In reality, I was offered a full scholarship to attend their program for a week. My first thought was "how much can we possibly learn in one week?" Remembering my experience at WUSTL, I decided to take the opportunity and I\'m really glad I did.');
+addParagraph(code, 'Prior to Codesmith, I had no experience coding in Javascript. I didn\'t realize that it was so closely linked to HTML and CSS, and I had no knowledge of the structure of a webpage. In the first few days, we started the day with a daily coding challenge. These were basically just general algorithm problems in javascript. We\'d then sit through a fundamental lecture and a daily set of challenges would be realeased. There may have been a lecture or two scheduled for the rest of the day, but for the most part, we spent the rest of the day working with a partner to complete the challenges. Throughout the week, there were also some incredibly inspirational guest speakers including an amazing entrepreneur/business man and lead engineers from Tinder, Slack, and Uber.');
+addParagraph(code, 'On the first day, we learned about how javascript compiles our code, and completed 20 challenges making algorithms to solve niche problems. The second day consisted of building an HTML page dynamically using javascript, and implementing APIs. The third day was spent using Facebook\'s React Framework to do the same thing. On the fourth day, we learned how to use Node.JS (specifically Express) to set up a server and host our website. Then the final three days were spent working on our independent projects with a partner. (You can read more about our running app in the "projects" page).');
+addParagraph(code, 'Overall, I\'d say Codesmith was another experience that highly impacted my life. After just one week, I now have the capability of making my own website, and putting my thoughts on the web for the entire world to see. I\'ve learned that software engineering is definitely something that I could enjoy doing, and even without formal schooling, I now have the foundation to continue learning on my own if needed. Codesmith has inspired me to put a greater emphasis on software in both my studies and personal projects. I am grateful to have gone through the Summer Academy and look forward to employing Codesmith\'s work-hard/play-hard mentality in everything I do from now on.');
 
 
 
