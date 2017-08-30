19169cac1faf61d6f8d0b64fc0ce164210ebda82
bernardnguyen <bernardquynguyen@gmail.com>
2017-08-30 15:46:47 -0700

    server

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
 var express = require('express');
 var app = express();
 
-// var edu = require('./edu/');
-// var exp = require('./exp/');
-// var proj = require('./proj/');
-// var rand = require('./rand/');
-
-
-/************** MIDDLEWARE ***************/
-
-function logreq(req, res, next){
-  console.log(req.method + req.url);
-  next();
-}
-
-function sendData(req, res, next){
-  if (req.url === '/education'){
-    res.send(edu);
-  } else if (req.url === '/experiences'){
-    res.send(exp);
-  } else if (req.url === '/projects'){
-    res.send(proj);
-  } else if (req.url === '/random'){
-    res.send(rand);
-  }
-}
-
 /************* DEFINE ROUTES *************/
-
-// app.get('/education', logreq, sendData);
-// app.get('/experiences', logreq, sendData);
-// app.get('/projects', logreq, sendData);
-// app.get('/random', logreq, sendData);
 
 app.get('/education', function(req, res) {
   res.sendfile('./edu.html');
